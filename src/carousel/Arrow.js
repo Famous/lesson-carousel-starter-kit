b179f2b09ba584847eaa0488d40632a006ec04a0
Ben Rowles <rowlesben@gmail.com>
2015-05-21 11:29:24 -0700

    updates

---
+++ src/carousel/Arrow.js
@@ -0,0 +1,23 @@
+var DOMElement = require('famous/dom-renderables/DOMElement');
+
+function Arrow(node, options) {
+  this.node = node;
+  this.el = new DOMElement(node);
+  this.el.setProperty('color', 'white')
+  this.direction = options.direction;
+  this.el.setContent(this.direction === 1 ? '>' : '<');
+  this.el.setProperty('fontSize', '40px');
+  this.el.setProperty('lineHeight', '40px');
+  this.el.setProperty('cursor', 'pointer');
+  this.el.setProperty('textHighlight', 'none');
+  this.el.setProperty('zIndex', '2');
+
+  function emitPageChange() {
+      this.node.emit('pageChange', this.direction);
+  }
+
+  this.gestures = new GestureHandler(node);
+  this.gestures.on('tap', emitPageChange);
+}
+
+module.exports = Arrow;
--- src/carousel/Carousel.js
+++ src/carousel/Carousel.js
@@ -1,5 +1,8 @@
 var FamousEngine = require('famous/core/FamousEngine');
 var DOMElement = require('famous/dom-renderables/DOMElement');
+var Arrow = require('./Arrow.js');
+// var Pager = require('./Pager.js');
+// var Dots = require('./Dots.js');
 
 function Carousel(selector, data) {
     this.context = FamousEngine.createScene(selector);
@@ -14,11 +17,11 @@ function Carousel(selector, data) {
         next: new Arrow(this.root.addChild(), { direction: 1 })
     };
 
-    this.pager = new Pager(this.root.addChild(), { pageData: this.pageData });
+    // this.pager = new Pager(this.root.addChild(), { pageData: this.pageData });
 
-    this.dots = new Dots(this.root.addChild(), { numPages: this.pageData.length });
+    // this.dots = new Dots(this.root.addChild(), { numPages: this.pageData.length });
 
-    _positionComponents.call(this);
+    // _positionComponents.call(this);
 }
 
 function _positionComponents() {
